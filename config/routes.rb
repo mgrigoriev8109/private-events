2ec54b0a5b56573e687571ce7280c10d7cce5bb1
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-18 11:17:08 -0400

    Set up Devise creating User MVC

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'events#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+
   resources :events
+
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'events#index'
 end
