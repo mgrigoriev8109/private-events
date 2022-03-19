510d498a71a53b906e6ed26870d88fc55954c07c
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-19 08:07:05 -0400

    Add #new #create and #strong_params to Event Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,4 +4,23 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(article_params)
+
+    if @event.save
+      redirect to @event
+    else
+      render :new, status :unprocessable_entity
+    end
+  end
+
+  private
+    def event_params
+      params.require(:event).permit(:name, :location, :date)
+    end
 end
