4d8646607118ece3dbe9236f0857d541144f1140
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-21 12:10:09 -0400

    Fix creation of user attendance
    Also adding uniquness validation to avoid duplication

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,3 +1,4 @@
+require 'debug'
 class EventAttendingsController < ApplicationController
   before_action :authenticate_user!, except: [:index] 
 
@@ -10,11 +11,11 @@ class EventAttendingsController < ApplicationController
   end
 
   def new
-    @event_attending = EventAttending.new
+    @event_attending = EventAttending.new()
   end
 
   def create
-    @event_attending = EventAttending.new(event_attending_params)
+    @event_attending = EventAttending.new(event_attendee_id: params[:event_attendee_id], attended_event_id: params[:attended_event_id])
 
     if @event_attending.save
       redirect_to events_path
@@ -22,9 +23,4 @@ class EventAttendingsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
-
-  private
-    def event_attending_params
-      params.require(:event_attending).permit(:event_attendee_id, :attended_event_id)
-    end
 end
--- app/models/event_attending.rb
+++ app/models/event_attending.rb
@@ -1,4 +1,6 @@
 class EventAttending < ApplicationRecord
-  belongs_to :created_event, class_name: "Event"
-  belongs_to :attended_event, class_name: "User"
+  belongs_to :created_event, class_name: "Event", optional: true
+  belongs_to :attended_event, class_name: "User", optional: true
+
+  validates :attended_event_id, uniqueness: { scope: :event_attendee_id}
 end
