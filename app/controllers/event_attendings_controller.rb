59623e6b829c6c5ba8354c4ab88fee5ee74576a0
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-21 21:30:42 -0400

    Create Event Show View to display event attendees

--- app/controllers/event_attendings_controller.rb
+++ app/controllers/event_attendings_controller.rb
@@ -1,4 +1,3 @@
-require 'debug'
 class EventAttendingsController < ApplicationController
   before_action :authenticate_user!, except: [:index] 
 
@@ -16,6 +15,7 @@ class EventAttendingsController < ApplicationController
 
   def create
     @event_attending = EventAttending.new(event_attendee_id: params[:event_attendee_id], attended_event_id: params[:attended_event_id])
+    #@event_attending = EventAttending.new(attending_params)
 
     if @event_attending.save
       redirect_to events_path
@@ -23,4 +23,9 @@ class EventAttendingsController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  private
+    def attending_params
+      params.require(:event_attendings).permit(event_attendee_id: params[:event_attendee_id], attended_event_id: params[:attended_event_id])
+    end
 end
