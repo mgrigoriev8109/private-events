b987ed60df0cf7961905f96454641abf4d7d47bd
mikhail <mikhailgrigoriev92@gmail.com>
2022-03-22 22:31:10 -0400

    Create Event Model Scopes for Past and Future Events
    Inserting logic into Event Index view to display all past and future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,12 @@
+require 'debug'
 class Event < ApplicationRecord
   has_many :event_attendings, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendings, source: :event_attendee
   belongs_to :creator, class_name: "User"
 
   validates :name, :location, :date, presence:true
+
+  scope :past, ->(current_date) { where("date < ?", current_date) }
+  scope :future, ->(current_date) { where("date > ?", current_date) }
+
 end
